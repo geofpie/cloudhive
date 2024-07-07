53be83a25894e48f4fd373e11890ff0b8441d408
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-07 15:15:03 +0800

    Update main.js
    refactor

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const spinnerWrapper = document.querySelector('.spinner-wrapper');
     const container = document.querySelector('.container-fluid');
 
+    // Event listeners for switching between login and register forms
     loginLink.addEventListener('click', function(event) {
         event.preventDefault();
         switchForm(loginForm, registerForm);
@@ -18,22 +19,31 @@ document.addEventListener('DOMContentLoaded', function() {
         switchActiveLink(registerLink, loginLink);
     });
 
-    window.addEventListener('load', function() {
-        spinnerWrapper.style.display = 'none';
-        container.classList.add('visible');
-    });
+    // Function to switch forms
+    function switchForm(formToShow, formToHide) {
+        formToShow.classList.add('fade-in', 'visible');
+        formToHide.classList.remove('visible', 'fade-in');
+        container.classList.add('fade-in');
+    }
 
+    // Function to switch active link
+    function switchActiveLink(activeLink, inactiveLink) {
+        activeLink.classList.add('active');
+        inactiveLink.classList.remove('active');
+    }
+
+    // Handle form submission for registration
     registerForm.addEventListener('submit', function(event) {
         event.preventDefault();
-    
+
         const username = document.getElementById('register-field-username').value;
         const email = document.getElementById('register-field-email').value;
         const password = document.getElementById('register-field-password').value;
         const registerButton = document.getElementById('register-button');
         const originalButtonText = registerButton.innerHTML;
-    
+
         showSpinner(registerButton);
-    
+
         fetch('/api/register', {
             method: 'POST',
             headers: {
@@ -49,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .then(data => {
             hideSpinner(registerButton, originalButtonText);
-    
+
             if (data.token) {
                 // Registration success with token
                 displayPopup('Registration Successful', 'text-success');
@@ -68,16 +78,17 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+    // Handle form submission for login
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault();
-    
+
         const identifier = document.getElementById('login-field-username').value; // Can be username or email
         const password = document.getElementById('login-field-password').value;
         const loginButton = document.getElementById('login-button');
         const originalButtonText = loginButton.innerHTML;
-    
+
         showSpinner(loginButton);
-    
+
         fetch('/api/login', {
             method: 'POST',
             headers: {
@@ -93,19 +104,46 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .then(data => {
             hideSpinner(loginButton, originalButtonText);
-    
+
             if (data.token) {
                 // Set token as a cookie
                 setCookie('token', data.token, 1); // Adjust expiry as needed (1 day in this case)
-    
-                // Redirect based on server response (handled by backend)
-                if (response.status === 302) {
-                    window.location.href = response.headers.get('Location');
-                    return;
-                }
-                
-                // Handle other cases (success scenario)
-                window.location.href = '/dashboard';
+
+                // Fetch user information
+                fetch('/api/fetchuserinfo', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${data.token}`
+                    }
+                })
+                .then(response => {
+                    if (response.status === 302) {
+                        // Handle redirect manually
+                        window.location.href = response.headers.get('Location'); // Redirect to the specified location
+                        return; // Exit fetch chain since we've redirected
+                    }
+                    if (!response.ok) {
+                        throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+                    }
+                    return response.json();
+                })
+                .then(userInfo => {
+                    // Check if required fields are filled
+                    if (!userInfo.first_name || !userInfo.last_name || !userInfo.country) {
+                        // Redirect to onboarding process
+                        window.location.href = '/onboarding';
+                    } else {
+                        // Redirect to dashboard
+                        window.location.href = '/dashboard';
+                    }
+                })
+                .catch(error => {
+                    // Handle error fetching user info
+                    console.error('Error fetching user info:', error);
+                    displayPopup('Failed to fetch user info', 'text-danger');
+                });
+
             } else {
                 // Login error (invalid credentials)
                 const errorMessage = data.error || 'Invalid credentials';
@@ -115,59 +153,46 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => {
             // Handle other login errors
             const errorMessage = error.message || 'Failed to login';
-            console.error('Login Error:', errorMessage); // Log the error message to console
-            if (error.response) {
-                console.error('Response Status:', error.response.status); // Log the response status code
-            }
             displayPopup(errorMessage, 'text-danger');
             hideSpinner(loginButton, originalButtonText);
         });
     });
-    
-    
+
+    // Function to set cookie
     function setCookie(name, value, days) {
         const expires = new Date();
         expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
         document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
     }
-    
 
-    function switchForm(formToShow, formToHide) {
-        formToShow.classList.add('fade-in', 'visible');
-        formToHide.classList.remove('visible', 'fade-in');
-        container.classList.add('fade-in');
-    }
-
-    function switchActiveLink(activeLink, inactiveLink) {
-        activeLink.classList.add('active');
-        inactiveLink.classList.remove('active');
-    }
-
+    // Function to show spinner
     function showSpinner(button) {
         button.innerHTML = '<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>';
         button.disabled = true;
     }
 
+    // Function to hide spinner
     function hideSpinner(button, originalText) {
         button.innerHTML = originalText;
         button.disabled = false;
     }
 
+    // Function to display popup message
     function displayPopup(message, className) {
         // Create popup element
         const popup = document.createElement('div');
         popup.classList.add('ajax-popup');
-        
+
         // Overlay background
         const overlay = document.createElement('div');
         overlay.classList.add('overlay');
         popup.appendChild(overlay);
-        
+
         // Popup content
         const popupContent = document.createElement('div');
         popupContent.classList.add('popup-content', className);
         popupContent.textContent = message;
-    
+
         // Close button
         const closeButton = document.createElement('button');
         closeButton.classList.add('close-button');
@@ -175,11 +200,11 @@ document.addEventListener('DOMContentLoaded', function() {
         closeButton.addEventListener('click', function() {
             popup.remove();
         });
-    
+
         // Append content and close button to popup
         popupContent.appendChild(closeButton);
         popup.appendChild(popupContent);
-    
+
         // Append popup to body
         document.body.appendChild(popup);
     }
