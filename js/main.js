b83a69e79586b3141098f78dde1cca2fafa24444
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-07 10:59:21 +0800

    Update main.js
    added onboarding flow, webtokens

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector('.container-fluid');
     const registerMessage = document.getElementById('register-message');
 
-    // Event listeners for switching between login and register forms
     loginLink.addEventListener('click', function(event) {
         event.preventDefault();
         switchForm(loginForm, registerForm);
@@ -20,26 +19,22 @@ document.addEventListener('DOMContentLoaded', function() {
         switchActiveLink(registerLink, loginLink);
     });
 
-    // Function to switch forms visibility
     function switchForm(formToShow, formToHide) {
         formToShow.classList.add('fade-in', 'visible');
         formToHide.classList.remove('visible', 'fade-in');
         container.classList.add('fade-in');
     }
 
-    // Function to switch active link
     function switchActiveLink(activeLink, inactiveLink) {
         activeLink.classList.add('active');
         inactiveLink.classList.remove('active');
     }
 
-    // Hide spinner when content is loaded
     window.addEventListener('load', function() {
         spinnerWrapper.style.display = 'none';
         container.classList.add('visible');
     });
 
-    // Event listener for register form submission
     registerForm.addEventListener('submit', function(event) {
         event.preventDefault();
 
@@ -49,10 +44,8 @@ document.addEventListener('DOMContentLoaded', function() {
         const registerButton = document.getElementById('register-button');
         const originalButtonText = registerButton.innerHTML;
 
-        // Show spinner and disable button
         showSpinner(registerButton);
 
-        // Perform AJAX request
         fetch('/api/register', {
             method: 'POST',
             headers: {
@@ -60,42 +53,40 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             body: JSON.stringify({ username, email, password }),
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to register user');
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
+            hideSpinner(registerButton, originalButtonText);
+
             if (data.message) {
                 // Registration success
                 registerMessage.innerText = data.message;
                 registerMessage.classList.remove('text-danger');
                 registerMessage.classList.add('text-success');
+
+                // Redirect to onboarding page
+                window.location.href = '/onboarding';
             } else {
                 // Registration error (username or email exists)
-                const errorMessage = data.error || 'Username or email already exists';
+                const errorMessage = data.error || 'Unknown Error';
                 registerMessage.innerText = errorMessage;
+                registerMessage.classList.remove('text-success');
                 registerMessage.classList.add('text-danger');
             }
         })
         .catch(error => {
             // Handle other errors
             registerMessage.innerText = error.message || 'Failed to register user';
+            registerMessage.classList.remove('text-success');
             registerMessage.classList.add('text-danger');
-        })
-        .finally(() => {
             hideSpinner(registerButton, originalButtonText);
         });
     });
 
-    // Function to show spinner and disable button
     function showSpinner(button) {
         button.innerHTML = '<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>';
         button.disabled = true;
     }
 
-    // Function to hide spinner and restore button text
     function hideSpinner(button, originalText) {
         button.innerHTML = originalText;
         button.disabled = false;
