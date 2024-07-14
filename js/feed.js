b57f36e98186aa12e66602315c69435f67b180c1
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-14 21:27:49 +0800

    Update feed.js

--- js/feed.js
+++ js/feed.js
@@ -25,8 +25,11 @@ function fetchUserInfo() {
 }
 
 function updateUserProfile(user) {
-    document.getElementById('hive-logged-in-user-name').innerText = user.first_name;
+    const loggedInUserName = document.getElementById('hive-logged-in-user-name');
+    const username = user.username;
+
+    loggedInUserName.innerText = user.first_name;
+    loggedInUserName.href = `/${username}`; 
+
     document.querySelector('.navbar-profile-pic').src = user.profile_picture_url;
-    var userProfileUrl = '/' + user.username;
-    document.getElementById('.hive-logged-in-user-name').setAttribute("href", userProfileUrl);
 }
