94ffa28e5c0476bf052a4b60b8ef2847e4bec3d4
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-24 23:51:32 +0800

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -553,24 +553,25 @@ document.addEventListener('DOMContentLoaded', function() {
             fetchPosts();
         }
     }); */
+
+    const editProfileButton = document.querySelector('.profile-stat.hive-user-action');
+    const editProfileModal = document.getElementById('editProfileModal');
+    const closeModalButton = document.getElementById('closeModal');
+
+    // Show the modal
+    editProfileButton.addEventListener('click', () => {
+        editProfileModal.classList.remove('hidden');
+    });
+
+    // Close the modal
+    closeModalButton.addEventListener('click', () => {
+        editProfileModal.classList.add('hidden');
+    });
 });
 
 document.getElementById('profilePicInput').addEventListener('change', handleProfilePicChange);
 document.getElementById('headerPicInput').addEventListener('change', handleHeaderPicChange);
 document.getElementById('submitEditProfileButton').addEventListener('click', submitEditProfile);
-const editProfileButton = document.querySelector('.profile-stat.hive-user-action');
-const editProfileModal = document.getElementById('editProfileModal');
-const closeModalButton = document.getElementById('closeModal');
-
-// Show the modal
-editProfileButton.addEventListener('click', () => {
-    editProfileModal.classList.remove('hidden');
-});
-
-// Close the modal
-closeModalButton.addEventListener('click', () => {
-    editProfileModal.classList.add('hidden');
-});
 
 let profilePicCropper, headerPicCropper;
 
