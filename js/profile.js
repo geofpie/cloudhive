b7321c1f543febed26d6cdf77e6eaca4fbbcc55a
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-25 20:35:09 +0800

    frontend changes

--- js/profile.js
+++ js/profile.js
@@ -557,3 +557,29 @@ dayjs.extend(dayjs_plugin_relativeTime);
 document.addEventListener('DOMContentLoaded', function() {
    
 });
+
+// Get the modal
+const modal = document.getElementById('editProfileModal');
+
+// Get the button that opens the modal
+const btn = document.getElementById('edit-profile');
+
+// Get the <span> element that closes the modal
+const span = document.getElementById('closeEditProfileModal');
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
