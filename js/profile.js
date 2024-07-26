9983d719248f0e3def4f026af1959e0006ee4dee
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-26 20:36:18 +0800

    ui palette changes

--- js/profile.js
+++ js/profile.js
@@ -564,16 +564,13 @@ const modal = document.getElementById('editProfileModal');
 // Get the button that opens the modal
 const btn = document.getElementById('edit-profile');
 
-// Get the <span> element that closes the modal
-const span = document.getElementById('closeEditProfileModal');
-
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = 'block';
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+document.getElementById('closeEditProfileModal').onclick = function() {
     modal.style.display = 'none';
 }
 
