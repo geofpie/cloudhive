e9a6c0a5d566b7056efa9899c5b15a87606c3512
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-27 14:12:30 +0800

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -308,7 +308,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     
                 handleImageLoad(); // Ensure this function is defined and properly handles image loading
     
-                isFetching = false;
             })
             .catch(error => {
                 console.error('Error fetching posts:', error);
