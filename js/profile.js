0d02a2a723d6555c3f500ec3798db63aa873adcc
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-27 17:06:59 +0800

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -553,23 +553,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function fetchUserInfo() {
-    fetch('/api/get_user_info', {
-        method: 'GET',
-        headers: {
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        updateUserProfile(data.userInfo);
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
+    updateUserProfile(data.userInfo);
 }
 
 function updateUserProfile(user) {
