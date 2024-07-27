c96591500df1037bec365897b282665f29b13061
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-27 12:49:13 +0800

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -291,8 +291,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 // Correctly target the like button icon and like count for the specific post
                 const postElement = document.querySelector(`.hive-post-element[data-post-id="${postId}"]`);
                 const likeButtonIcon = postElement.querySelector(`#like-btn-hive`);
-                const likeButtonIconSrc = data.message === 'Like added' ? '../assets/liked.svg' : '../assets/unliked.svg';
-                likeButtonIcon.src = likeButtonIconSrc;
+                const likeButton = postElement.querySelector('.hive-stat-like-btn');
+                
+                // Toggle the liked class based on response
+                if (data.message === 'Like added') {
+                    likeButton.classList.add('liked');
+                    likeButtonIcon.src = '../assets/liked.svg'; // Change to liked icon
+                } else {
+                    likeButton.classList.remove('liked');
+                    likeButtonIcon.src = '../assets/unliked.svg'; // Change to unliked icon
+                }
 
                 // Update like count
                 const likeCountElement = postElement.querySelector('.hive-stat-like strong');
