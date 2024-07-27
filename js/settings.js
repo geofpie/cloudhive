1a35e3bb364c9c592d40615482bf18ff0b7dff98
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-27 17:14:56 +0800

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -52,19 +52,43 @@ function fetchUserInfo() {
     fetch('/api/get_user_info', {
         method: 'GET',
     })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('user-fullname').innerText = `${data.first_name} ${data.last_name}`;
-        document.getElementById('user-username').innerText = `@${data.username}`;
-        document.getElementById('user-email').innerText = data.email;
-        document.querySelector('.user-info-card img').src = data.profile_picture_url;
-        document.getElementById('hive-logged-in-user-name').innerText = `${data.first_name}`;
-        document.getElementById('hive-logged-in-user-name').href = `/${data.username}`;
-        document.querySelector('.navbar-profile-pic').src =`${data.profile_picture_url}`;
+    .then(response => {
+        if (response.status === 401) {
+            // Redirect to homepage if user is unauthorized
+            window.location.href = '/';
+            return; // Stop further processing
+        }
+        
+        if (!response.ok) {
+            throw new Error('Error fetching user info: ' + response.statusText);
+        }
+        
+        return response.json();
     })
-    .catch(error => console.error('Error fetching user info:', error));
+    .then(data => {
+        if (data.redirect) {
+            // Handle redirect instructions from the server
+            window.location.href = data.redirect;
+        } else {
+            updateUserProfileFields(data); // Call the separate function to update fields
+        }
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        // Optionally redirect to homepage on catch error
+        window.location.href = '/';
+    });
 }
 
+function updateUserProfileFields(data) {
+    document.getElementById('user-fullname').innerText = `${data.first_name} ${data.last_name}`;
+    document.getElementById('user-username').innerText = `@${data.username}`;
+    document.getElementById('user-email').innerText = data.email;
+    document.querySelector('.user-info-card img').src = data.profile_picture_url;
+    document.getElementById('hive-logged-in-user-name').innerText = `${data.first_name}`;
+    document.getElementById('hive-logged-in-user-name').href = `/${data.username}`;
+    document.querySelector('.navbar-profile-pic').src = data.profile_picture_url;
+}
 
 const notificationsModal = document.getElementById('notificationsModal');
 // When the user clicks anywhere outside of the modal, close it
