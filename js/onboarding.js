81b8cf8ce8ebbe4be9e633c5c59680eda2b32e9a
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-28 14:24:27 +0800

    Update onboarding.js

--- js/onboarding.js
+++ js/onboarding.js
@@ -71,13 +71,17 @@ document.addEventListener('DOMContentLoaded', function() {
             image.onload = function() {
                 // Initialize Cropper.js
                 cropper = new Cropper(image, {
-                    aspectRatio: 1, // Set to 1 for square aspect ratio
+                    dragMode: "move",
+                    aspectRatio: 1 / 1, // Set to 1 for square aspect ratio
                     viewMode: 1, // Set to 1 for preview mode, allowing user manipulation
                     autoCropArea: 1, // Set to 0.8 for initial crop area size (80% of the image)
                     movable: false, // Disable user movement of the crop box
                     zoomable: false, // Allow user to zoom the image
                     rotatable: false, // Disable image rotation
                     scalable: false, // Disable image scaling
+                    cropBoxResizable: false,
+                    minContainerHeight: "500",
+                    minCanvasHeight: "500",
                     ready: function() {
                         console.log('Cropper.js initialized.');
                     }
