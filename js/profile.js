1536d7e17dd226095a8df30ba2dae1a060100872
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-28 17:12:31 +0800

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -604,9 +604,7 @@ btn.onclick = function() {
 
 // When the user clicks on <span> (x), close the modal
 document.getElementById('closeEditProfileModal').onclick = function(event) {
-    modal.style.display = 'none';
-    document.body.classList.remove('modal-open');
-    document.getElementById('editProfileModal').classList.remove('show');
+    modal.classList.add('hidden');
 }
 
 // When the user clicks anywhere outside of the modal, close it
